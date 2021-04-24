20c7bf5e5188255bb39cd721822672d80cb6e8ab
hello there <>
2021-04-24 09:35:13 +0000

    Initial commit

---
+++ mongodb.py
@@ -0,0 +1,53 @@
+import pymongo
+import dns
+import os
+from flask import session
+from werkzeug.security import generate_password_hash, check_password_hash
+clientm = os.getenv("clientm")
+mainclient = pymongo.MongoClient(clientm)
+usersdb = mainclient.Profiles
+profilescol = usersdb.Users
+
+def addcookie(key, value):
+  session[key] = value
+
+def delcookie(keyname):
+  session.clear()
+
+def getcookie(key):
+  try:
+    if (x := session.get(key)):
+      return x
+    else:
+      return False
+  except:
+    return False
+
+def makeaccount(username, password):
+  passhash = generate_password_hash(password)
+  document = [{
+    "Username": username,
+    "Password": passhash,
+    "Money": 0,
+    "Drinks": 0,
+    "Menu": {}
+  }]
+  profilescol.insert_many(document)
+
+def gethashpass(username):
+  for user in profilescol.find():
+    if user['Username'] == username:
+      return user['Password']
+  return False
+
+def getuser(username):
+  for user in profilescol.find():
+    if user['Username'] == username:
+      return user
+  return False
+
+def allusers():
+  users = []
+  for user in profilescol.find():
+    users.append(user['Username'].lower())
+  return users
